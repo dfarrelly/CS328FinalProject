293c8059c8fde5a0e61217a043f886d0c97dcba0
AMMayberry1 <ammayberry1@gmail.com>
2017-09-06 15:16:30 -0400

    Initial commit

---
+++ app/src/main/java/edu/umass/cs/cs328_a0/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.umass.cs.cs328_a0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
